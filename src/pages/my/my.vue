ff42a6d1b2708f1c655db9c04f5251bd30bf4fb4
Array-Zheng <2105206234@qq.com>
2020-12-09 17:44:33 +0800

    第三次提交

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -3,18 +3,29 @@
         <view class="content-header">
             <view>
                 <image mode="widthFix" src="/static/logo.png"></image>
-                <text>小然然</text>
+                <text>你好啊</text>
             </view>
            
         </view>
 		<view class="content-center">
-        
+            <uni-list>
+                <uni-list-item thumb="/static/images/qiandao.png" :showArrow="true"  title="每日签到" ></uni-list-item>
+                <uni-list-item thumb="/static/images/shouchang.png" :showArrow="true" title="我的收藏" ></uni-list-item>
+                <uni-list-item thumb="/static/images/youhui.png" :showArrow="true"  title="优惠买单" ></uni-list-item>
+                <uni-list-item thumb="/static/images/dingdan.png" :showArrow="true" title="全部订单" ></uni-list-item>
+                <uni-list-item thumb="/static/images/address.png" :showArrow="true"  title="收货地址" ></uni-list-item>
+                <uni-list-item thumb="/static/images/kefu.png" :showArrow="true"  title="联系商家" ></uni-list-item>
+                <uni-list-item thumb="/static/images/women.png" :showArrow="true" title="关于我们" ></uni-list-item>
+            </uni-list>
         </view>
 	</view>
 </template>
 
 <script>
+import uniList from '../../components/uni-list/uni-list.vue'
+    //导入列表组件
 	export default {
+  components: { uniList },
 		data() {
 			return {
 				
@@ -25,7 +36,8 @@
 		},
 		methods: {
 
-		}
+        }
+       
 	}
 </script>
 
@@ -51,8 +63,5 @@
         float: left;
         border: 1px solid red;
     }
-    .content-center{
-        width: 100%;
-        height: 120rpx;
-    }
+    
 </style>
